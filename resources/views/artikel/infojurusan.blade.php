b4735ef55865989d652fe524ddd0ae2a9ee0ccbc
Pradita Cahyani <praditagra@gmail.com>
2022-04-18 07:22:55 +0700

    fix artikel views

--- resources/views/artikel/infojurusan.blade.php
+++ resources/views/artikel/infojurusan.blade.php
@@ -6,17 +6,17 @@ active
 <div class=" p-5 bg-body shadow" style="border-radius: 25px">
 
         <div class="container">
-            <h3 class="card-title fw-bold">Jurusan {{ $data_jurusan ['jurusan'] }}</h3>
+            <h3 class="card-title fw-bold" style="color:#666666">Jurusan {{ $data_jurusan ['nama_jurusan'] }}</h3>
             <p class="text-justify text-muted mt-3">{{ $data_jurusan ['desc_jurusan'] }}</p>  
         </div>
         <div class="container mt-5">
-            <h3 class="card-title fw-bold">Kenapa Harus Memilih Jurusan {{ $data_jurusan ['nama_jurusan'] }}? </h3>
+            <h3 class="card-title fw-bold" style="color:#666666">Kenapa Harus Memilih Jurusan {{ $data_jurusan ['nama_jurusan'] }}? </h3>
             <div class="row">
                 <div class="col-6 ">
                     <div class="bg-body shadow mb-3 border" style="border-radius : 25px">
                         <div class="row g-0 align-data_jurusans-center ">
                           <div class="col-md-2 ">
-                            <h1 class="fw-bold text-center position-relative">1.</h1>
+                            <h1 style="color:#666666" class="fw-bold text-center position-relative">1.</h1>
                           </div>
                           <div class="col-md-10">
                             <div class="card-body">
@@ -30,7 +30,7 @@ active
                     <div class="bg-body shadow border" style="border-radius : 25px">
                         <div class="row g-0 align-data_jurusans-center">
                           <div class="col-md-2">
-                            <h1 class="fw-bold text-center position-relative">2.</h1>
+                            <h1 style="color:#666666" class="fw-bold text-center position-relative">2.</h1>
                           </div>
                           <div class="col-md-10">
                             <div class="card-body">
@@ -44,7 +44,7 @@ active
                     <div class="bg-body shadow mb-3 border" style="border-radius : 25px">
                         <div class="row g-0 align-data_jurusans-center">
                           <div class="col-md-2">
-                            <h1 class="fw-bold text-center position-relative">3.</h1>
+                            <h1 style="color:#666666" class="fw-bold text-center position-relative">3.</h1>
                           </div>
                           <div class="col-md-10">
                             <div class="card-body">
@@ -58,7 +58,7 @@ active
                     <div class="bg-body shadow mb-3 border" style="border-radius : 25px">
                         <div class="row g-0 align-data_jurusans-center">
                           <div class="col-md-2">
-                            <h1 class="fw-bold text-center position-relative">4.</h1>
+                            <h1 style="color:#666666" class="fw-bold text-center position-relative">4.</h1>
                           </div>
                           <div class="col-md-10">
                             <div class="card-body">
@@ -71,7 +71,7 @@ active
             </div>
         </div>
         <div class="container mt-5">
-            <h3 class="card-title fw-bold">Prospek Kerja Jurusan {{ $data_jurusan ['nama_jurusan'] }}</h3>
+            <h3 class="card-title fw-bold" style="color:#666666">Prospek Kerja Jurusan {{ $data_jurusan ['nama_jurusan'] }}</h3>
             <center>
             <div class="row">
                 <div class="col-2  text-center">
