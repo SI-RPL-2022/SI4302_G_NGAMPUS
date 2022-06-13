61f64ab103fc5b5dccbcd780feacd5a10fa6697a
vimelya <virzamelati861@gmail.com>
2022-06-13 14:54:50 +0700

    admin home ref #11

--- resources/views/adminHome.blade.php
+++ resources/views/adminHome.blade.php
@@ -3,17 +3,135 @@
 actived
 @endsection
 @section('konten')
-<br><br><br>
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-                <div class="card-body">
-                    You are Admin.
+
+<!-- dashboard -->
+<section class="section dashboard">
+        <div class="row">
+
+            <!-- Left side columns -->
+            <div class="col">
+                <div class="row">
+                    <!-- Total Produk Card-->
+                    <div class="col-xxl-4 col-md-3">
+                        <div class="card info-card total-card">
+
+                            <div class="card-body">
+                                
+                                <div class="d-flex align-items-center pt-3">
+
+                                <div class="ps-3 pb-2 ">
+                                    <h6 style="color:#00B5BF"> </h6>
+                                    <h5 class="card-title">Total Produk</h5>
+                                </div>
+                                <div class="filter p-3">
+                                    <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                                        <i class="bi bi-cart"></i>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
+                <!-- End Total Produk Card -->
+
+
+<!-- dashboard -->
+<section class="section dashboard">
+        <div class="row">
+
+            <!-- Left side columns -->
+            <div class="col">
+                <div class="row">
+                    <!-- Total Produk Card-->
+                    <div class="col-xxl-4 col-md-3">
+                        <div class="card info-card total-card">
+
+                            <div class="card-body">
+                                
+                                <div class="d-flex align-items-center pt-3">
+
+                                <div class="ps-3 pb-2 ">
+                                    <h6 style="color:#00B5BF"> </h6>
+                                    <h5 class="card-title">Produk</h5>
+                                </div>
+                                <div class="filter p-3">
+                                    <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                                        <i class="bi bi-cart"></i>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- End Total Produk Card -->
+
+
+
+<!-- dashboard -->
+<section class="section dashboard">
+        <div class="row">
+
+            <!-- Left side columns -->
+            <div class="col">
+                <div class="row">
+                    <!-- Total Produk Card-->
+                    <div class="col-xxl-4 col-md-3">
+                        <div class="card info-card total-card">
+
+                            <div class="card-body">
+                                
+                                <div class="d-flex align-items-center pt-3">
+
+                                <div class="ps-3 pb-2 ">
+                                    <h6 style="color:#00B5BF"> </h6>
+                                    <h5 class="card-title">Artikel</h5>
+                                </div>
+                                <div class="filter p-3">
+                                    <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                                        <i class="bi bi-cart"></i>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- End Total Produk Card -->
+
+
+
+<main id="main" class="main">
+    <div class="pagetitle">
+        <h1>Overview</h1>
+            <nav>
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="/admin">Beranda</a></li>
     </div>
-</div>
+<!-- End Page Title -->
+
+<!-- tabel -->
+<div class="content">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+
+                <div class="card-body">
+                    
+                    <div class="table-responsive ">
+                        <table class="table">
+                            <thead style="color:#00B5BF">
+                            <tr>
+                                <th scope="col">No</th>
+                                <th scope="col">ID Registrasi</th>
+                                <th scope="col">Nama</th>
+                                <th scope="col">Nomor Telepon</th>
+                                <th scope="col">Email</th>
+                                <th scope="col">Status</th>
+                                <th scope="col">Action</th>
+                            </tr>
+                            </thead>
+
+                            <tbody>
 @endsection
